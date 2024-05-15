50011a66368256b7043bcec0a9cbf1522b52188e
hayong39 <yia0524_hy@naver.com>
2024-05-15 14:56:30 +0900

    [0515]섹션3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
+from django.shortcuts import render
 from django.http import (HttpResponse, HttpResponseRedirect)
 from django.urls import reverse
+
 # Create your views here.
 
 def index(request):
@@ -7,10 +9,17 @@ def index(request):
     #return HttpResponseRedirect('1')
     #return HttpResponseRedirect(
     #    reverse('detail', args=[1]))
-    return HttpResponseRedirect(
-        reverse('detail', kwargs={'question_id':1})
-    )
-
+    #return HttpResponseRedirect(
+    #    reverse('detail', kwargs={'question_id':1}))
+    ctx = {
+        "greetings" : "Hello there!",
+        "location" : {
+            "city" : "Seoul",
+            "country" : "South Korea"
+        },
+        "languages" : ["Korean", "English"]
+    }
+    return render(request, 'polls/main.html', context = ctx)
 def detail(request, question_id):
     return HttpResponse("You're looking at question%s" %question_id)
 def results(request, question_id):
